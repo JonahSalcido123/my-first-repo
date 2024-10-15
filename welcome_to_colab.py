e91d09a8e1e48cd6e67d64bccb4b49341e75cdd6
Jonah Salcido <156844294+JonahSalcido123@users.noreply.github.com>
2024-10-15 14:54:52 -0600

    Update welcome_to_colab.py

--- welcome_to_colab.py
+++ welcome_to_colab.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/notebooks/intro.ipynb
 """
 
-!pip install streamlit
-
 # Commented out IPython magic to ensure Python compatibility.
 # %%writefile app.py
 # import streamlit as st
