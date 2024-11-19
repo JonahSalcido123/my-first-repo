66e6f764b749973d0ce03ebfa734635139bce525
Jonah Salcido <156844294+JonahSalcido123@users.noreply.github.com>
2024-11-19 14:24:28 -0700

    Update python_colab_code.py.py

--- python_colab_code.py.py
+++ python_colab_code.py.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1gLtTioch21KDLv7cHBKxlZJ8vJ9drUna
 """
 
-! pip install streamlit
-
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
